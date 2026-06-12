6a4fa986e5d2e3329d5b72e75af55bcf8b3e047c
Vishal <vishalsingh4328m@gmail.com>
2026-06-12 12:25:46 +0530

    added multiplication

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,10 @@
 public class Calculator {
 
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Calculator Application");
+        System.out.println("Multiplication: " + multiply(5, 3));
     }
 }
